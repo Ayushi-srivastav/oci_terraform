99023f8764164aaa94a5d4149a18597e1e3aeff6
Ayushi <ayushisrivastav@gmail.com>
2026-01-08 17:35:13 -0400

    create IG

---
+++ gateway.tf
@@ -0,0 +1,10 @@
+resource "oci_core_internet_gateway" "flip_internet_gateway" {
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaa3xzm37emmmjjamq5uo25ohrfdlytd6cb2qlhaksc65temewbdipa"  #flip-network-compartment OCID
+  vcn_id         = "ocid1.vcn.oc1.iad.amaaaaaaugo2acaayhrrey236o2d3flcrkgnc7thl7jvdjcnixhos2iy3ava"   #flip-vcn OCID
+  display_name   = "flip-internet-gateway"  #Internet Gateway name for flip VCN
+  enabled        = true
+
+  defined_tags = {
+    "FLIPKART-TAGS.Environment" = "Prod"
+  }
+}
