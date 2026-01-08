877e135706c99c80d9ed64fdde4d3968ad49bc93
Ayushi <ayushisrivastav@gmail.com>
2026-01-08 18:00:15 -0400

    change name

---
+++ security-list.tf
@@ -0,0 +1,27 @@
+# security list for public subnet
+resource "oci_core_security_list" "public_sl" {
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaa3xzm37emmmjjamq5uo25ohrfdlytd6cb2qlhaksc65temewbdipa"
+  vcn_id         = "ocid1.vcn.oc1.iad.amaaaaaaugo2acaayhrrey236o2d3flcrkgnc7thl7jvdjcnixhos2iy3ava"
+  display_name   = "flip-public-security-list"
+
+  ingress_security_rules {
+    protocol    = "6"   # 6 is the protocol number for TCP
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    description = "Allow SSH from all sources"
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+    description = "Allow all outbound traffic"
+  }
+
+  defined_tags = {
+    "FLIPKART-TAGS.Environment" = "Prod"
+  }
+}
